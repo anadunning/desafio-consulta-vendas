5505bd84009c1eb4b110206890b59e31aa5a915d
anadunning <anakpedrosa@hotmail.com>
2025-09-19 22:21:17 -0300

    SaleDTO created

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,22 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsmeta.dto.SaleDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	@Query("SELECT new com.devsuperior.dsmeta.dto.SaleDTO(obj.id, obj.date, obj.amount, obj.name) FROM Sale obj"
+			+ " WHERE obj.date BETWEEN :start AND :end "
+			+ " AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))"
+			+ "GROUP BY obj.seller.name")
+	List<SaleDTO> getReport(LocalDate start, LocalDate end, String name);
+	
+	
 
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,4 +23,6 @@ public class SaleService {
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+	
+	
 }
